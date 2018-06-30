e24b48fee2ab20d185022828a287346f689de157
Puneeth <puneethsai90@gmail.com>
2018-06-30 13:00:00 +0530

    Love Calculator Commit-1

--- app/src/main/java/com/example/puneeth/lovecalculatorteen/Constants/RestAPI.java
+++ app/src/main/java/com/example/puneeth/lovecalculatorteen/Constants/RestAPI.java
@@ -8,6 +8,4 @@ public class RestAPI {
 
     public static final String loveCaluclatorURL= "https://love-calculator.p.mashape.com/getPercentage?";
     //fname=Bokki&sname=Potti";
-
-    public static final String quoteGeneratorUrl = "https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous&count=30";
-}
+ }
